60a10d60153edaa178baafd8a54905669937eece
João Fernandes <ojoaofernandes@gmail.com>
2017-11-16 03:17:05 -0200

    Remove user id field from announcement form and get it from logged user

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -26,6 +26,7 @@ class AnnouncementsController < ApplicationController
   # POST /announcements.json
   def create
     @announcement = Announcement.new(announcement_params)
+    @announcement.user_id = current_user.id
 
     respond_to do |format|
       if @announcement.save
