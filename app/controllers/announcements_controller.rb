6a2bc08474be7251a4aebd065134b1b8e7614d9a
João Fernandes <ojoaofernandes@gmail.com>
2017-11-16 03:10:29 -0200

    Add auth requirement to Announcement controller

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -1,4 +1,5 @@
 class AnnouncementsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_announcement, only: [:show, :edit, :update, :destroy]
 
   # GET /announcements
